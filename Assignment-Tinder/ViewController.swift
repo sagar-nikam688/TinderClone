dfbede836d5486f6701808bd3a5681c4b443afb9
Sagar Nikam <s.nikam@bifishbenefits.com>
2020-09-19 16:19:02 +0530

    Initial Commit

---
+++ Assignment-Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-Tinder
+//
+//  Created by Admin on 19/09/20.
+//  Copyright © 2020 sagar nikam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
